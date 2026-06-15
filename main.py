1a9064e5451f6be35e8e98de4039069e8acce0d7
FiorellaGatti <fioreng.96@gmail.com>
2026-06-14 23:44:53 -0300

    agrega función cargar_csv

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import csv
+def cargar_csv(nombre_archivo): #Función para leer el archivo csv y crear la lista de diccionarios
+    try:
+        with open(nombre_archivo, "r", encoding="utf-8") as archivo:#abre el archivo en modo reader            
+            lector_archivo = csv.DictReader(archivo) #Con DictReader crea un diccionario con el contenido del csv
+            lista_paises=[]
+            for fila in lector_archivo:#itera con un for el diccionario creado
+                pais = { #Cada fila del diccionario inicial será un nuevo diccionario por país
+                    "nombre": fila["nombre"],
+                    "poblacion": int(fila["poblacion"]),#convierte los elementos de población y superficie a entero para poder utilizarlos luego
+                    "superficie": int(fila["superficie"]),
+                    "continente": fila["continente"]
+                }
+                lista_paises.append(pais)#agrega a la nueva lista el nuevo diccionario
+        return lista_paises
+    except FileNotFoundError: #Manejo de errores por archivo inexistente o si algún dato no pudo convertirse a entero
+            print("El archivo que intenta abrir no existe")
+    except ValueError:
+            print("Error de formato en el CSV")
+    
+
